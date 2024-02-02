c71654bf95030974a84aaa94061f1b388fd904c9
root <root@ip-172-31-28-255.ec2.internal>
2024-02-02 12:14:24 +0000

    Spotify Server Terraform

--- SpotifyMain.tf
+++ SpotifyMain.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "xxxxxxxx"
-  secret_key = "xxxxxxxx"
+  access_key = "xxxxxxx"
+  secret_key = "xxxxxxx"
 }
 
-resource "aws_instance" "SERVER" {
+resource "aws_instance" "SPOTIFY" {
   ami           = "ami-05fb0b8c1424f266b"
   instance_type = "t2.micro"
 
@@ -14,16 +14,13 @@ resource "aws_instance" "SERVER" {
               apt-get install -y docker.io
               docker pull sanjaym08/spotify:v5
               docker run -d -p 80:80 sanjaym08/spotify:v5
-              docker pull sanjaym08/loverboy_youtube_site
-              docker run -d -p 443:80 sanjaym08/loverboy_youtube_site
               EOF
 
   tags = {
-    Name = "SERVER"
+    Name = "SPOTIFY"
   }
 }
 
 output "public_ip" {
-  value = aws_instance.SERVER.public_ip
+  value = aws_instance.SPOTIFY.public_ip
 }
-
